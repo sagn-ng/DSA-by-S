97d4f3c74fc8acdc3c7c4cefbd683cd231ddcdba
sagn-ng <sangnn2510@gmail.com>
2026-08-20 00:22:41 +0700

    - created .gitignore - removed self-created c header files that cause errors to the compiler - Errors fixed in Matrix Chain Multiplication

--- src/C_Cpp/MatrixChainMulti.cpp
+++ src/C_Cpp/MatrixChainMulti.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 using namespace std;
-int minMultiCost(vector<int> arr, int i, int j, vector<vector<int>> c){
+int minMultiCost(vector<int> &arr, int i, int j, vector<vector<int>> &c){
     if (i==j) return 0;
 
     if (c[i][j] != -1) return c[i][j];
@@ -10,7 +11,7 @@ int minMultiCost(vector<int> arr, int i, int j, vector<vector<int>> c){
 
     for (int k=i; k<j; k++){
         int curr = minMultiCost(arr, i, k, c) + minMultiCost(arr, k+1, j, c)
-        + arr[i]*arr[k]*arr[j];
+        + arr[i-1]*arr[k]*arr[j];
 
         m = min(curr, m);
     }
@@ -19,10 +20,19 @@ int minMultiCost(vector<int> arr, int i, int j, vector<vector<int>> c){
     return m;
 }
 
-int matrixMulti(vector<int> arr){
+int matrixMulti(vector<int> &arr){
     int n=arr.size();
+    if (n<=1) return 0;
 
     vector<vector<int>> c(n, vector<int>(n, -1));
 
-    return minMultiCost(arr, 0, n-1, c);
+    return minMultiCost(arr, 1, n-1, c);
+}
+
+int main()
+{
+    vector<int> arr = {2, 1, 3, 4};
+    int res = matrixMulti(arr);
+    cout << res << endl;
+    return 0;
 }
