dbe8e428a943cc6a818404d574b29d0f8bb1dfb0
sagn-ng <sangnn2510@gmail.com>
2026-08-21 23:49:46 +0700

    Optimal BST (continue) - unsuccessful search included (learned from Abdul Bari); There is another answer due to how we deal with the case (i==j): some might return q[i], others (me also) might return 0

--- src/C_Cpp/OptimalBST.cpp
+++ src/C_Cpp/OptimalBST.cpp
@@ -24,10 +24,34 @@ int minBSTSuccessful(vector<int> &p){
     return c[0][n];
 } //tabulation
 
+//unsuccessful search
+int minBSTUnsuccessful(vector<vector<int>> &dp, vector<int> &p, vector<int> &q, int i, int j){
+    if (i==j) return 0;
 
+    if (dp[i][j]!=-1) return dp[i][j];
+
+    dp[i][j]=INT_MAX;
+
+    for (int r=i+1; r<=j; r++){
+        int left_cost=minBSTUnsuccessful(dp, p, q, i, r-1);
+        int right_cost=minBSTUnsuccessful(dp, p, q, r, j);
+        dp[i][j]=min(dp[i][j], left_cost+right_cost);
+    }
+
+    int w_range=q[i];
+    for (int r=i+1; r<=j; r++) w_range += (p[r-1]+q[r]);
+
+    dp[i][j]+=w_range;
+    return dp[i][j];
+}
 
 int main(){
     vector<int> p_test1={4,2,6,3};
-    cout<<minBSTSuccessful(p_test1);
+    cout<<"An example for successful search only: "<<minBSTSuccessful(p_test1)<<'\n';
+
+    vector<int> p_test2={3,3,1,1};
+    vector<int> q={2,3,1,1,1};
+    vector<vector<int>> dp(5, vector<int>(5, -1));
+    cout<<"An example for including unsuccessful search: "<<minBSTUnsuccessful(dp, p_test2, q, 0, 4);
     return 0;
 }
