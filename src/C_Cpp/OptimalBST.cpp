61e3bb72b815a4523942fb6ff5869b353ae36093
sagn-ng <sangnn2510@gmail.com>
2026-08-21 23:27:56 +0700

    updated the solution using tabulation for Optimal Binary Search Tree (successful search only)

--- src/C_Cpp/OptimalBST.cpp
+++ src/C_Cpp/OptimalBST.cpp
@@ -3,34 +3,31 @@
 #include <vector>
 using namespace std;
 //successful search only
-int optimalBST(vector<int> &keys, vector<int> &p){
-    int n=keys.size();
-    vector<vector<int>> c(n, vector<int>(n)); //n x n table
+int minBSTSuccessful(vector<int> &p){
+    int n=p.size();
+    vector<vector<int>> c(n+1, vector<int>(n+1)); //(n+1) x (n+1) table
 
-    for (int diff=0; diff<=n-1; diff++){ //j - i = 0, 1,...,n-1
-        for (int i=0; i+diff<=n-1; i++){
+    for (int diff=1; diff<=n; diff++){
+        for (int i=0; i+diff<=n; i++){
             int j=i+diff;
-            int p_range=0, min_temp = INT_MAX;
+            int p_range=0, min_temp=INT_MAX;
 
-            for (int r=i; r<=j; r++){
-                p_range+=p.at(r);
-                
-                if (r==0) min_temp=min(min_temp, c.at(1).at(j));
-                else if (r==n-1) min_temp=min(min_temp, c.at(i).at(n-2));
+            for (int r=i+1; r<=j; r++){
+                p_range+=p[r-1]; //i, r, j in c<> are 1-indexed
 
-                else min_temp=min(min_temp, c.at(i).at(r-1)+c.at(r+1).at(j));
+                min_temp=min(min_temp, c[i][r-1]+c[r][j]);
             }
-
-            c.at(i).at(j)=p_range+min_temp;
+            c[i][j]=p_range+min_temp;
         }
     }
 
-    return c[0][n-1];
-} //tabulation, but making c as a (n+1) x (n+1) table would be easier
+    return c[0][n];
+} //tabulation
+
+
 
 int main(){
-    vector<int> keys={10,20,30,40};
-    vector<int> p={4,2,6,3};
-    cout<<optimalBST(keys, p);
+    vector<int> p_test1={4,2,6,3};
+    cout<<minBSTSuccessful(p_test1);
     return 0;
 }
